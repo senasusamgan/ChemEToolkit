ded7af471c56b4e2023154da3a9e07ec8bac5a43
senasusamgan <senasu.samgan@gmail.com>
2026-07-13 22:26:57 +0300

    Initial Commit

---
+++ ChemEToolkit/ChemEToolkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChemEToolkitApp.swift
+//  ChemEToolkit
+//
+//  Created by Sena Su Samgan on 13.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct ChemEToolkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
